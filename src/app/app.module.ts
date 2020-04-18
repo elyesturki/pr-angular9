3e5f2ef8b8a2cf5af1a01acd67220f9b572ca7e2
Elyes Turki <elyestu@gmail.com>
2020-04-19 01:41:48 +0200

    12-sprint 1 - init project cvTech, integration template et component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { GrandChildComponent } from './grand-child/grand-child.component';
 import { ColorComponent } from './color/color.component';
 import { FormsModule } from '@angular/forms';
 import { FilsComponent } from './fils/fils.component';
+import { CvComponent } from './cvTech/cv/cv.component';
+import { ListeCvComponent } from './cvTech/liste-cv/liste-cv.component';
+import { ItemCvComponent } from './cvTech/item-cv/item-cv.component';
+import { DetailCvComponent } from './cvTech/detail-cv/detail-cv.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { FilsComponent } from './fils/fils.component';
     ChildBComponent,
     GrandChildComponent,
     ColorComponent,
-    FilsComponent
+    FilsComponent,
+    CvComponent,
+    ListeCvComponent,
+    ItemCvComponent,
+    DetailCvComponent
   ],
   imports: [
     BrowserModule,
