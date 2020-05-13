cf7eaace81a2f48f7b6f708aba12604bb6516c6f
Elyes Turki <elyestu@gmail.com>
2020-05-13 06:41:27 +0200

    52- interceptor - correction avec methode 3 - récupere tous les anciens params et rajoute le token - utilise keys, get

--- src/app/interceptors/login.interceptor.ts
+++ src/app/interceptors/login.interceptor.ts
@@ -7,11 +7,23 @@ export class loginInterceptor implements HttpInterceptor {
     //console.log("Requette interceptor: ",req)
     const token: string = localStorage.getItem('token');
     if(token) {
+      const keys = req.params.keys();
+      console.log("keys params existants: ",keys);
+      let params = new HttpParams();
+      for (let i = 0; i < keys.length; i++) {
+        params = params.append(keys[i], req.params.get(keys[i]));
+      }
       const cloneReq = req.clone({
-        setParams: {
-          'access_token': token
-        }
-       // params: new HttpParams().set('access_token', token)
+        /* --- Methode 3- récupere tous les anciens params et rajoute le token (success si on est logged) --- */
+        params: params.append('access_token', token)
+
+        /* --- Methode 2- Rajoute un nv params token et marche bien (success si on est logged) --- */
+        // setParams: {
+        //   'access_token': token
+        // }
+
+        // --- Methode 1- Ecrase les params (erreur si on est logged) --- */
+        // params: new HttpParams().set('access_token', token)
       });
       return next.handle(cloneReq);
     } else {
